ada227915bc44190cef602b6a907c68b1596b466
martin-swe-fs <297412436+martin-swe-fs@users.noreply.github.com>
2026-07-10 14:56:56 +0200

    chore(frontend): add very_good_analysis with custom rules

--- frontend/test/rl_app_test.dart
+++ frontend/test/rl_app_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:frontend/rl_app.dart';
 
 void main() {
-  testWidgets('RlApp renders scaffold', (final WidgetTester tester) async {
+  testWidgets('RlApp renders scaffold', (tester) async {
     await tester.pumpWidget(const RlApp());
 
     expect(find.byType(Scaffold), findsOneWidget);
